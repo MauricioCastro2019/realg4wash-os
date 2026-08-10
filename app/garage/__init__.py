3aea12c69a6f706b74debd7e30c0eae5b2682424
MauricioCastro2019 <52500276+MauricioCastro2019@users.noreply.github.com>
2026-08-10 15:57:54 -0600

    feat: register CenterFix pilot console

--- app/garage/__init__.py
+++ app/garage/__init__.py
@@ -4,3 +4,4 @@ from flask import Blueprint
 garage_bp = Blueprint("garage", __name__, url_prefix="/garage")
 
 from . import routes  # noqa: E402,F401
+from . import provider_routes  # noqa: E402,F401
